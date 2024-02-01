4e28927cbabbf83e524b2b3c692afabb1450d912
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-02-01 22:48:56 +0200

    add middleware

--- middleware/isEmptyBody.js
+++ middleware/isEmptyBody.js
@@ -1,2 +1,11 @@
-const isEmptyBody = (req, res, next) => {};
+import { HttpError } from "../helpers/index.js";
+
+const isEmptyBody = (req, res, next) => {
+  const items = Object.keys(req.body);
+  const length = items.length;
+  if (length === 0) {
+    return next(HttpError(400, "missing fields"));
+  }
+  next();
+};
 export default isEmptyBody;
--- middleware/isValidId.js
+++ middleware/isValidId.js
@@ -1,2 +1,11 @@
-const isValidId = (req, res, next) => {};
+import { HttpError } from "../helpers/index.js";
+import { isValidObjectId } from "mongoose";
+
+const isValidId = (req, res, next) => {
+  const { id } = req.params;
+  if (isValidObjectId(id) === false) {
+    return next(HttpError(400, `${id} is not id`));
+  }
+  next();
+};
 export default isValidId;
