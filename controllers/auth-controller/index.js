89ff9fe8c1d7eec15e5ef969ef8e7ebaf93e5321
OlegShinka <volkonxxl@gmail.com>
2024-02-03 23:05:34 +0200

    add currentUser/updateUser

--- controllers/auth-controller/index.js
+++ controllers/auth-controller/index.js
@@ -1,2 +1,4 @@
 export { default as register } from "./register.js";
 export { default as signin } from "./signin.js";
+export { default as logout } from "./logout.js";
+export { default as currentUser } from "./current.js";
