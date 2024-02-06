5e3ebe95cacf9f85468201b263a97c648782b8b5
Kateryna-gr <kategrytsyk@gmail.com>
2024-02-06 22:33:56 +0200

    cards-and-mail-fix

--- controllers/cards-controller/getAllCards.js
+++ controllers/cards-controller/getAllCards.js
@@ -3,7 +3,7 @@ import Card from "../../models/Card.js";
 const getAllCards = async (req, res) => {
   const { idColumn } = req.params;
 
-  const result = await Card.find({ owner: idColumn });
+  const result = await Card.find({ owner: idColumn }).populate("owner", "title");
 
   res.json(result);
 };
