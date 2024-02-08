3873c953e95b3b0715cc6a7212b96cb20122f102
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-02-08 20:03:21 +0200

    fix

--- controllers/columns-controller/deleteByIdColumn.js
+++ controllers/columns-controller/deleteByIdColumn.js
@@ -1,8 +1,10 @@
 import { HttpError } from "../../helpers/index.js";
+import Card from "../../models/Card.js";
 import Column from "../../models/Column.js";
 
 const deleteByIdColumn = async (req, res) => {
   const { id: _id, idBoard: owner } = req.params;
+  await Card.deleteMany({ owner: _id });
   const deleteColumn = await Column.findOneAndDelete({ _id, owner });
   if (deleteColumn === null) {
     throw HttpError(404);
